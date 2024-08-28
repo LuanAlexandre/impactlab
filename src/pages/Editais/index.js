46144c3e5010bb66171189fdd4217fa21f43df48
Luan Alexandre <luanrb19@gmail.com>
2024-08-27 22:29:15 -0400

    Atualiza pagina de editais

--- src/pages/Editais/index.js
+++ src/pages/Editais/index.js
@@ -3,6 +3,7 @@ import { MainContent } from './styled';
 import { Container } from 'react-bootstrap';
 import edital from '../../files/edital_001_2024/projeto_Impact-Lab_edital_001_2024_selecao_discentes.pdf';
 import { forms } from '../../assets/config/formulario';
+import homologacao from '../../files/edital_001_2024/Projeto_Impact-Lab_Edital_001_2024_homologacao_inscricoes.pdf';
 
 export default function Editais() {
   return (
@@ -28,6 +29,24 @@ export default function Editais() {
                     <a href={forms.url} target="_blank" rel="noreferrer">
                       <h4>Formulário de Requerimento de Inscrição</h4>
                     </a>
+                  </div>
+                  <div className="mb-4">
+                    <div className="small text-muted">27 de Agosto, 2024</div>
+                    <a href={homologacao} target="_blank" rel="noreferrer">
+                      <h4>Homologação das Inscrições</h4>
+                    </a>
+                  </div>
+                  <div className="mb-4">
+                    <div className="small text-muted">28 de Agosto, 2024</div>
+                    {/*<a href="" target="_blank" rel="noreferrer">*/}
+                    <h4>Resultado Preliminar</h4>
+                    {/*</a>*/}
+                  </div>
+                  <div className="mb-4">
+                    <div className="small text-muted">31 de Agosto, 2024</div>
+                    {/*<a href="" target="_blank" rel="noreferrer">*/}
+                    <h4>Resultado Final</h4>
+                    {/*</a>*/}
                   </div>
                 </div>
               </div>
